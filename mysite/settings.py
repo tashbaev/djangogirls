228d5e4f07d290b948553a642a810d5a15cc9133
Tugolbay <t.tashbaev@gmail.com>
2021-11-05 11:48:41 +0600

    sqllite

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,16 +78,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djangogirls_db',
-        'USER': 'tugolbay',
-        'PASSWORD': '1',
-        'HOST': 'localhost',
-        'PORT': 5432
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 
 # Password validation
